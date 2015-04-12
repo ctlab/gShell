891293c02fcf7faf7635538f54a98be30346c0eb
Arseniy Seroka <ars.seroka@gmail.com>
2015-04-13 00:22:19 +0300

    impl: work folders, remove: addTo*

--- src/State.hs
+++ src/State.hs
@@ -6,9 +6,7 @@ module State ( GState (..)
              , projectRoot
              , gshellRoot
              , commitsRoot
-             , addToProject
-             , addToGshell
-             , addToCommits
+             , workFolders
              , viewProjectRoot
              , viewGshellRoot
              , viewCommitsRoot
@@ -19,18 +17,11 @@ import           Folders
 
 import           Control.Applicative
 import           Control.Lens
-import           Data.Default
-import           Data.Text
-import qualified Data.Text.IO          as T
 import           System.Directory
 import           System.Directory.Tree
-import           System.IO.Error
 
-type GState = AnchoredDirTree Text
-type GDir = DirTree Text
-
-addToFolder :: Setting (->) s t [a] [a] -> [a] -> s -> t
-addToFolder lens what = over lens (++ what)
+type GState = AnchoredDirTree FilePath
+type GDir = DirTree FilePath
 
 filteredByName
   :: (Choice p, Applicative f) =>
@@ -42,9 +33,6 @@ projectRoot :: Control.Applicative.Applicative f =>
      -> GState -> f (GState)
 projectRoot = _dirTree._contents
 
-addToProject :: [GDir] -> GState -> GState
-addToProject = addToFolder projectRoot
-
 viewProjectRoot :: GState -> [GDir]
 viewProjectRoot = view projectRoot
 
@@ -52,9 +40,6 @@ gshellRoot :: Control.Applicative.Applicative f =>
      ([GDir] -> f [GDir])
      -> GState -> f (GState)
 gshellRoot = projectRoot.traverse.filteredByName gshellFolderName._contents
-
-addToGshell :: [GDir] -> GState -> GState
-addToGshell = addToFolder gshellRoot
 
 viewGshellRoot :: GState -> [GDir]
 viewGshellRoot = view gshellRoot
@@ -64,11 +49,12 @@ commitsRoot :: Control.Applicative.Applicative f =>
      -> GState -> f (GState)
 commitsRoot = gshellRoot.traverse.filteredByName commitsFolderName._contents
 
-addToCommits :: [GDir] -> GState -> GState
-addToCommits = addToFolder commitsRoot
-
 viewCommitsRoot :: GState -> [GDir]
 viewCommitsRoot = view commitsRoot
 
+workFolders :: Applicative f =>
+         (GDir -> f (GDir)) -> GState -> f GState
+workFolders = projectRoot.traverse.filtered ((== workFolderName) . (Prelude.take (Prelude.length workFolderName)) . (^. _name))
+
 generateState :: FilePath -> IO GState
-generateState = readDirectoryWithL T.readFile
+generateState = build
