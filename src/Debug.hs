8b1856c6f506121289d82887787564891d57c753
Arseniy Seroka <ars.seroka@gmail.com>
2015-04-13 00:22:46 +0300

    modify: debug

--- src/Debug.hs
+++ src/Debug.hs
@@ -1,9 +1,13 @@
 module Debug ( debug
              , printDebug
+             , ifDebug
              ) where
 
 debug :: Bool
 debug = True
 
-printDebug :: String -> IO ()
+printDebug :: Show a => a -> IO ()
 printDebug a = if debug then print a else return ()
+
+ifDebug :: a -> a -> a
+ifDebug a b = if debug then a else b
