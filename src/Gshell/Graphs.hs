67e3a27cf6e3e2b6746f931d1e0422f6f377158a
Arseniy Seroka <ars.seroka@gmail.com>
2015-05-22 20:28:06 +0300

    impr: graph building + omit empty

--- src/Gshell/Graphs.hs
+++ src/Gshell/Graphs.hs
@@ -32,32 +32,37 @@ printGraph state = do
 
 buildGraph :: GState -> IO (Gr (Text, Shape) StyleName)
 buildGraph state = do
-    nodes <- mkNodes state
-    let edges = mkEdges state
+    let revisions = sortBy (cmpRevs state)
+            $ filter (\revName -> not $ null $ state ^. revCommit revName)
+            $ state ^.. commitsRoot.traverse._name
+    nodes <- mkNodes state revisions
+    let edges = mkEdges state revisions
     return $ mkGraph nodes edges
 
-mkNodes :: GState -> IO ([(Int, (Text, Shape))])
-mkNodes state = do
-    contents <- mapM (liftM (content . intercalate "\n" . filter (\a -> a /= "." && a /= "..")) . getDirectoryContents . mountDir (projectPath state)) revisions
+mkNodes :: GState -> [FilePath] -> IO ([(Int, (Text, Shape))])
+mkNodes state revisions = do
+    contents <- mapM (
+        liftM (content . intercalate "\n" . filter (\a -> a /= "." && a /= ".."))
+        . getDirectoryContents
+        . mountDir (projectPath state))
+        revisions
     let commands = map (\a -> command $ state ^. revCommit a) revisions
     return $ zip [0..] $ concat $ transpose [commands, contents]
-    where
-        revisions = sortBy (cmpRevs state) $ state ^.. commitsRoot.traverse._name
 
-mkEdges :: GState -> [(Int, Int, StyleName)]
-mkEdges state = result
+mkEdges :: GState -> [FilePath] -> [(Int, Int, StyleName)]
+mkEdges state revisions' = concatMap (filter (\(a, b, _) -> a /= b + 1) . join . ($ revisions)) [parRels, commandRels, openRels]
     where
-        revisions = flip zip [0..] $ sortBy (cmpRevs state) $ state ^.. commitsRoot.traverse._name
-        getCommandByNum n = n * 2
-        getContentByNum n = n * 2 + 1
-        commandRels = zipWith (\a b -> [(a, b, Solid)]) (map (getCommandByNum . snd) revisions) (map (getContentByNum . snd) revisions)
-        parRels = concatMap (connect (pars) Dotted) revisions
-        pars revName = (read $ state ^. parents revName) ^. parentsRevs
-        openRels = concatMap (connect (opens) Solid) revisions
-        opens revName = M.keys $ getReadLog revName state
+        revisions = flip zip [0..] revisions'
+        commandRels revisions = (map (\(_, num) -> [(getCommandByNum num, getContentByNum num, Solid)]) revisions)
+        parRels = (connect pars Dotted =<<)
+        openRels = (connect opens Solid =<<)
+        pars = (^. parentsRevs) . read . (state ^.) . parents
+        opens = M.keys . flip getReadLog state
         connect suppers lineStyle (revName, num) = map (\p -> [(getContentByNum $ getNum p, getCommandByNum num, lineStyle)]) $ suppers revName
-        getNum p = M.findWithDefault (-1) p $ M.fromList revisions
-        result = concat $ map join [parRels, commandRels, openRels]
+        getNum = ($ M.fromList revisions) . M.findWithDefault (-1)
+
+getCommandByNum n = n * 2
+getContentByNum n = n * 2 + 1
 
 graphParams  :: GraphvizParams t (Text, Shape) StyleName () (Text, Shape)
 graphParams = nonClusteredParams { globalAttributes = attrs, fmtNode = fn, fmtEdge = fe }
